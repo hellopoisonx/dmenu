581d188c4cdfbc58f5bacac8b9c7f9e9624deae2
hellopoisonx <x1665341912@gmail.com>
2024-12-31 22:56:40 +0800

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,89 +1,77 @@
 {
   description = "dmenu";
 
-  # Nixpkgs / NixOS version to use.
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim.url = "github:hellopoisonx/nixvim";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixvim,
-      ...
-    }:
-    let
-
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
-
-      # System types to support.
-      supportedSystems = [
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
+      systems = [
         "x86_64-linux"
-        "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
+        "x86_64-darwin"
       ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          final,
+          ...
+        }:
+        let
+          lastModifiedDate = self'.lastModifiedDate or self'.lastModified or "19700101";
+          version = builtins.substring 0 8 lastModifiedDate;
+        in
+        {
+          overlayAttrs = { };
+          packages.dmenu = final.stdenv.mkDerivation {
+            pname = "dmenu";
+            inherit version;
 
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      nixpkgsFor = forAllSystems (
-        system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        }
-      );
-    in
-    {
-
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
-        dmenu = final.stdenv.mkDerivation {
-          pname = "dmenu";
-          inherit version;
-
-          src = ./.;
-          buildInputs = with final; [
-            xorg.libX11
-            xorg.libXinerama
-            xorg.libXft
-          ];
-          nativeBuildInputs = with final; [
-            pkg-config
-            makeWrapper
-            nixvim.packages.${final.system}.c-cpp
-            bear
-          ];
-          preConfigure = ''
-            sed -i "s@/usr/local@$out@" config.mk
-            sed -ri -e 's!\<(dmenu|dmenu_path|stest)\>!'"$out/bin"'/&!g' dmenu_run
-            sed -ri -e 's!\<stest\>!'"$out/bin"'/&!g' dmenu_path
-            makeFlagsArray+=(
-              CC="$CC"
-              INCS="`$PKG_CONFIG --cflags fontconfig x11 xft xinerama`"
-              LIBS="`$PKG_CONFIG --libs   fontconfig x11 xft xinerama`"
-            )
-          '';
-          buildPhase = ''
-            make clean
-            make all
-          '';
-          installPhase = ''
-            make clean install
-          '';
+            src = ./.;
+            buildInputs = with final; [
+              xorg.libX11
+              xorg.libXinerama
+              xorg.libXft
+            ];
+            nativeBuildInputs = with final; [
+              pkg-config
+              makeWrapper
+              inputs'.nixvim.c-cpp
+              bear
+            ];
+            preConfigure = ''
+              sed -i "s@/usr/local@$out@" config.mk
+              sed -ri -e 's!\<(dmenu|dmenu_path|stest)\>!'"$out/bin"'/&!g' dmenu_run
+              sed -ri -e 's!\<stest\>!'"$out/bin"'/&!g' dmenu_path
+              makeFlagsArray+=(
+                CC="$CC"
+                INCS="`$PKG_CONFIG --cflags fontconfig x11 xft xinerama`"
+                LIBS="`$PKG_CONFIG --libs   fontconfig x11 xft xinerama`"
+              )
+            '';
+            buildPhase = ''
+              make clean
+              make all
+            '';
+            installPhase = ''
+              make clean install
+            '';
+          };
+          packages.default = self'.packages.dmenu;
         };
-
+      flake = {
       };
-
-      packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) dmenu;
-      });
-      defaultPackage = forAllSystems (system: self.packages.${system}.dmenu);
     };
 }
